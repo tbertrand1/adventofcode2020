19af6ece0cde5cb592eae09da1c377f1d4f12bc1
Thomas BERTRAND <tbertrand1@gmail.com>
2020-12-07 14:26:35 +0100

    Day 07

---
+++ days/day07/day07.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+
+	files "../../utils/files"
+)
+
+const filename = "day07.txt"
+const shinyGold = "shiny gold"
+
+type Color string
+
+type Rule struct {
+	color  Color
+	number int
+}
+
+type Graph map[Color][]Rule
+
+func main() {
+	inputs := files.ReadLinesOfFile(filename)
+
+	fmt.Printf("Part 1: %d\n", part1(inputs))
+	fmt.Printf("Part 2: %d\n", part2(inputs))
+}
+
+func part1(inputs []string) int {
+	graph := buildGraph(inputs)
+	nbBags := 0
+	for color := range graph {
+		if graph.CanContains(color, shinyGold) {
+			nbBags++
+		}
+	}
+	return nbBags
+}
+
+func part2(inputs []string) int {
+	graph := buildGraph(inputs)
+	return graph.CountBags(shinyGold)
+}
+
+func buildGraph(inputs []string) Graph {
+	graph := make(Graph)
+	regexLine := regexp.MustCompile("^([a-z]+ [a-z]+) bags contain (.*)\\.$")
+	regexRule := regexp.MustCompile("([0-9]) ([a-z]+ [a-z]+) bag")
+	for _, input := range inputs {
+		lineMatches := regexLine.FindStringSubmatch(input)
+		lineColor := Color(lineMatches[1])
+		ruleMatches := regexRule.FindAllStringSubmatch(lineMatches[2], -1)
+		for _, rule := range ruleMatches {
+			ruleColor := Color(rule[2])
+			ruleNumber, _ := strconv.Atoi(rule[1])
+			graph[lineColor] = append(graph[lineColor], Rule{color: ruleColor, number: ruleNumber})
+		}
+	}
+	return graph
+}
+
+func (graph Graph) CanContains(color Color, expected Color) bool {
+	for _, rule := range graph[color] {
+		if rule.color == expected {
+			return true
+		}
+		if graph.CanContains(rule.color, expected) {
+			return true
+		}
+	}
+	return false
+}
+
+func (graph Graph) CountBags(color Color) int {
+	count := 0
+	for _, rule := range graph[color] {
+		count += rule.number * (1 + graph.CountBags(rule.color))
+	}
+	return count
+}
---
+++ days/day07/day07_test.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"testing"
+
+	files "../../utils/files"
+	tests "../../utils/tests"
+)
+
+func testInputs() []string {
+	return []string{
+		"light red bags contain 1 bright white bag, 2 muted yellow bags.",
+		"dark orange bags contain 3 bright white bags, 4 muted yellow bags.",
+		"bright white bags contain 1 shiny gold bag.",
+		"muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.",
+		"shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.",
+		"dark olive bags contain 3 faded blue bags, 4 dotted black bags.",
+		"vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.",
+		"faded blue bags contain no other bags.",
+		"dotted black bags contain no other bags.",
+	}
+}
+
+func TestPart1WithTestInputs(t *testing.T) {
+	tests.AssertEquals(t, "part1()", 4, part1(testInputs()))
+}
+
+func TestPart1(t *testing.T) {
+	inputs := files.ReadLinesOfFile(filename)
+	tests.AssertEquals(t, "part1()", 103, part1(inputs))
+}
+
+func TestPart2WithTestInputs(t *testing.T) {
+	tests.AssertEquals(t, "part2()", 32, part2(testInputs()))
+}
+
+func TestPart2(t *testing.T) {
+	inputs := files.ReadLinesOfFile(filename)
+	tests.AssertEquals(t, "part2()", 1469, part2(inputs))
+}
