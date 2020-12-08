4805ab5799f25b86e81ec7f66df3267dd4bf6f34
Thomas BERTRAND <tbertrand1@gmail.com>
2020-12-08 18:44:36 +0100

    Day 08

---
+++ days/day08/day08.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"../../utils/files"
+)
+
+const filename = "day08.txt"
+
+func main() {
+	inputs := files.ReadLinesOfFile(filename)
+
+	fmt.Printf("Part 1: %d\n", part1(inputs))
+	fmt.Printf("Part 2: %d\n", part2(inputs))
+}
+
+func part1(inputs []string) int {
+	handheldConsole := buildHandheldConsole(inputs)
+	accumulator, status := handheldConsole.execute()
+	if status == loop {
+		return accumulator
+	}
+	panic("Wrong status")
+}
+
+func part2(inputs []string) int {
+	handheldConsole := buildHandheldConsole(inputs)
+
+	for index, instruction := range handheldConsole.instructions {
+		if instruction.operation == jumpOperation || instruction.operation == noOperation {
+			originalOperation := instruction.operation
+			if originalOperation == jumpOperation {
+				handheldConsole.updateInstructionOperation(index, noOperation)
+			} else {
+				handheldConsole.updateInstructionOperation(index, jumpOperation)
+			}
+
+			accumulator, end := handheldConsole.execute()
+			if end == success {
+				return accumulator
+			}
+
+			handheldConsole.updateInstructionOperation(index, originalOperation)
+		}
+	}
+	panic("Cannot found a solution")
+}
+
+type handheldConsole struct {
+	instructions map[int]*instruction
+}
+
+type instruction struct {
+	operation operation
+	argument  int
+}
+
+type operation string
+
+const accumulatorOperation = operation("acc")
+const jumpOperation = operation("jmp")
+const noOperation = operation("nop")
+
+type executionStatus string
+
+const loop = executionStatus("LOOP")
+const success = executionStatus("SUCCESS")
+
+func buildHandheldConsole(inputs []string) handheldConsole {
+	handheldConsole := handheldConsole{}
+	handheldConsole.instructions = make(map[int]*instruction)
+
+	for index, input := range inputs {
+		fields := strings.Fields(input)
+		operation := operation(fields[0])
+		argument, _ := strconv.Atoi(fields[1])
+		handheldConsole.instructions[index] = &instruction{operation: operation, argument: argument}
+	}
+
+	return handheldConsole
+}
+
+func (hc *handheldConsole) execute() (int, executionStatus) {
+	index := 0
+	accumulator := 0
+	indexExecuted := make(map[int]bool)
+
+	for {
+		currentIndex := index
+		if currentIndex == len(hc.instructions) {
+			return accumulator, success
+		}
+
+		instruction := hc.instructions[currentIndex]
+		_, executed := indexExecuted[currentIndex]
+		if executed {
+			return accumulator, loop
+		}
+
+		switch instruction.operation {
+		case accumulatorOperation:
+			accumulator += instruction.argument
+			index++
+		case jumpOperation:
+			index += instruction.argument
+		case noOperation:
+			index++
+		default:
+			panic("Operation not supported")
+		}
+
+		indexExecuted[currentIndex] = true
+	}
+}
+
+func (hc *handheldConsole) updateInstructionOperation(index int, operation operation) {
+	hc.instructions[index].operation = operation
+}
---
+++ days/day08/day08_test.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"testing"
+
+	"../../utils/files"
+	"../../utils/tests"
+)
+
+func testInputs() []string {
+	return []string{
+		"nop +0",
+		"acc +1",
+		"jmp +4",
+		"acc +3",
+		"jmp -3",
+		"acc -99",
+		"acc +1",
+		"jmp -4",
+		"acc +6",
+	}
+}
+
+func TestPart1WithTestInputs(t *testing.T) {
+	tests.AssertEquals(t, "part1()", 5, part1(testInputs()))
+}
+
+func TestPart1(t *testing.T) {
+	inputs := files.ReadLinesOfFile(filename)
+	tests.AssertEquals(t, "part1()", 1766, part1(inputs))
+}
+
+func TestPart2WithTestInputs(t *testing.T) {
+	tests.AssertEquals(t, "part2()", 8, part2(testInputs()))
+}
+
+func TestPart2(t *testing.T) {
+	inputs := files.ReadLinesOfFile(filename)
+	tests.AssertEquals(t, "part2()", 1639, part2(inputs))
+}
